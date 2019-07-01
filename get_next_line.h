2714e76ae72b5ae7e106276676a356c0020448c3
Matthew GERBER <magerber@c3r2s5.wethinkcode.co.za>
2019-07-01 10:35:27 +0200

    dsa

--- get_next_line.h
+++ get_next_line.h
@@ -6,15 +6,15 @@
 /*   By: magerber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/20 13:47:05 by magerber          #+#    #+#             */
-/*   Updated: 2019/06/26 13:28:06 by magerber         ###   ########.fr       */
+/*   Updated: 2019/07/01 10:29:03 by magerber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 1000
+# define BUFF_SIZE 1
 
-# include "Libft/libft.h"
+# include "libft/libft.h"
 
 int		get_next_line(const int fd, char **line);
 
